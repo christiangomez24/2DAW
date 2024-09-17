814387555a4c83a1a4a8dbcd80b4fe0278c17066
christiangomez24 <thechristiangm@gmail.com>
2024-09-17 19:50:09 +0200

    añadido ejercicio area circulo

---
+++ servidor/PHP/ejercicios1/area_circulo.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $radio = 3.5;
+        define('PI',3.1416);
+        $area_circulo = PI*($radio*$radio);
+    ?>
+    <p>El area del círculo es <?php echo $area_circulo;?></p>
+</body>
+</html>
